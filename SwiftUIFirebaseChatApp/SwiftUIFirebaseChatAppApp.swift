3e595f3d1b3851491109655c4021ac01ffb06641
iosmitDev <mitesh.imply@gmail.com>
2026-07-19 17:54:08 +0530

    Initial Commit

---
+++ SwiftUIFirebaseChatApp/SwiftUIFirebaseChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatAppApp.swift
+//  SwiftUIFirebaseChatApp
+//
+//  Created by MiteshKumar Patel on 19/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
